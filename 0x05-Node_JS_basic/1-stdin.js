4a0363713148747090bb1617fb123a2d20c279aa
qassim156 <59389885+qassim156@users.noreply.github.com>
2024-11-26 12:23:49 +0100

    stdin_js_upload

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,8 +6,8 @@ process.stdin.on('readable', () => {
   if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
+  
 });
-
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
